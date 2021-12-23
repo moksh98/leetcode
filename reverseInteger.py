fa5f3ad6be911e69a60f02d4e9a8a1e02025f6b6
moksh98 <mokshjain98@gmail.com>
2021-12-22 20:45:00 -0500

    Solution to Reverse Integer

---
+++ reverseInteger.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        if x < 0:
+            return -self.reverseDigit(abs(x))
+        elif x > 0:
+            return self.reverseDigit(x)
+        else:
+            return 0
+    def reverseDigit(self, x):
+        rev = 0
+        while x > 0:
+            rev = (rev*10) + x%10
+            x = x//10
+        if rev < (-2 ** 31) or rev > ((2 ** 31) - 1):
+            return 0
+        return rev
