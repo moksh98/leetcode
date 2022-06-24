145061577e4a1b69a2651b2285f26461e1633688
Moksh Jain <mokshjain98@gmail.com>
2022-06-23 20:46:58 -0400

    Time: 382 ms (5.18%), Space: 32.2 MB (13.79%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.py
@@ -0,0 +1,28 @@
+import numpy as np
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        m = len(image)
+        n = len(image[0])
+        def dfs(image, sr, sc, old_color, color, visited):
+            # print("image: ", image)
+            # print("Visited: ", visited)
+            # print("sr: ", sr)
+            # print("sc: ", sc)
+            if sr > m-1 or sc > n-1 or sr < 0 or sc < 0:
+                return
+            elif visited[sr][sc] == 1:
+                return
+            elif image[sr][sc] != old_color:
+                return
+            else:
+                visited[sr][sc] = 1
+                image[sr][sc] = color
+                dfs(image, sr+1, sc, old_color, color, visited)
+                dfs(image, sr-1, sc, old_color, color, visited)
+                dfs(image, sr, sc+1, old_color, color, visited)
+                dfs(image, sr, sc-1, old_color, color, visited)
+        visited = np.zeros((m, n))
+        # print(visited)
+        dfs(image, sr, sc, image[sr][sc], color, visited)
+        return image
+        
