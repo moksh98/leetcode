05293d15f3339bf9c76aaba48df8199622075284
Moksh Jain <mokshjain98@gmail.com>
2022-06-23 20:48:34 -0400

    Time: 260 ms (5.18%), Space: 32 MB (13.79%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -4,15 +4,9 @@ class Solution:
         m = len(image)
         n = len(image[0])
         def dfs(image, sr, sc, old_color, color, visited):
-            # print("image: ", image)
-            # print("Visited: ", visited)
-            # print("sr: ", sr)
-            # print("sc: ", sc)
             if sr > m-1 or sc > n-1 or sr < 0 or sc < 0:
                 return
-            elif visited[sr][sc] == 1:
-                return
-            elif image[sr][sc] != old_color:
+            elif visited[sr][sc] == 1 or image[sr][sc] != old_color:
                 return
             else:
                 visited[sr][sc] = 1
@@ -22,7 +16,6 @@ class Solution:
                 dfs(image, sr, sc+1, old_color, color, visited)
                 dfs(image, sr, sc-1, old_color, color, visited)
         visited = np.zeros((m, n))
-        # print(visited)
         dfs(image, sr, sc, image[sr][sc], color, visited)
         return image
         
