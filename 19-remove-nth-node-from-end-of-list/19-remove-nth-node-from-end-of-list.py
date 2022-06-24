540954f91b1da375bd568019c4a1d277a6b55fd3
Moksh Jain <mokshjain98@gmail.com>
2022-06-23 20:05:39 -0400

    Time: 32 ms (95.14%), Space: 13.8 MB (68.85%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.py
@@ -3,24 +3,38 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+# class Solution:
+#     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
+#         length = 0
+#         start = head
+#         while start != None:
+#             start = start.next
+#             length += 1
+#         if n == length:
+#             return head.next
+#         k = length - n
+#         count = 0
+#         start = head
+#         while count <= k:
+#             if count == k-1:
+#                 start.next = start.next.next
+#                 count += 1
+#             else:
+#                 start = start.next
+#                 count += 1
+#         return head
+
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
-        length = 0
-        start = head
-        while start != None:
-            start = start.next
-            length += 1
-        if n == length:
+        front = head
+        back = head
+        for i in range(n):
+            front = front.next
+        if front == None:
             return head.next
-        k = length - n
-        count = 0
-        start = head
-        while count <= k:
-            if count == k-1:
-                start.next = start.next.next
-                count += 1
-            else:
-                start = start.next
-                count += 1
+        while front.next != None:
+            front = front.next
+            back = back.next
+        back.next = back.next.next
         return head
-        
+            
