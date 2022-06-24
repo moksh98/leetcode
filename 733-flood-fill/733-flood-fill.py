176bc4e15e6f2ff8db5aa44eafbec51d654db426
Moksh Jain <mokshjain98@gmail.com>
2022-06-23 20:49:59 -0400

    Time: 85 ms (81.42%), Space: 14.2 MB (13.79%) - LeetHub

--- 733-flood-fill/733-flood-fill.py
+++ 733-flood-fill/733-flood-fill.py
@@ -1,21 +1,36 @@
-import numpy as np
-class Solution:
-    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        m = len(image)
-        n = len(image[0])
-        def dfs(image, sr, sc, old_color, color, visited):
-            if sr > m-1 or sc > n-1 or sr < 0 or sc < 0:
-                return
-            elif visited[sr][sc] == 1 or image[sr][sc] != old_color:
-                return
-            else:
-                visited[sr][sc] = 1
-                image[sr][sc] = color
-                dfs(image, sr+1, sc, old_color, color, visited)
-                dfs(image, sr-1, sc, old_color, color, visited)
-                dfs(image, sr, sc+1, old_color, color, visited)
-                dfs(image, sr, sc-1, old_color, color, visited)
-        visited = np.zeros((m, n))
-        dfs(image, sr, sc, image[sr][sc], color, visited)
+# import numpy as np
+# class Solution:
+#     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+#         m = len(image)
+#         n = len(image[0])
+#         def dfs(image, sr, sc, old_color, color, visited):
+#             if sr > m-1 or sc > n-1 or sr < 0 or sc < 0:
+#                 return
+#             elif visited[sr][sc] == 1 or image[sr][sc] != old_color:
+#                 return
+#             else:
+#                 visited[sr][sc] = 1
+#                 image[sr][sc] = color
+#                 dfs(image, sr+1, sc, old_color, color, visited)
+#                 dfs(image, sr-1, sc, old_color, color, visited)
+#                 dfs(image, sr, sc+1, old_color, color, visited)
+#                 dfs(image, sr, sc-1, old_color, color, visited)
+#         visited = np.zeros((m, n))
+#         dfs(image, sr, sc, image[sr][sc], color, visited)
+#         return image
+class Solution(object):
+    def floodFill(self, image, sr, sc, newColor):
+        R, C = len(image), len(image[0])
+        color = image[sr][sc]
+        if color == newColor: return image
+        def dfs(r, c):
+            if image[r][c] == color:
+                image[r][c] = newColor
+                if r >= 1: dfs(r-1, c)
+                if r+1 < R: dfs(r+1, c)
+                if c >= 1: dfs(r, c-1)
+                if c+1 < C: dfs(r, c+1)
+
+        dfs(sr, sc)
         return image
         
