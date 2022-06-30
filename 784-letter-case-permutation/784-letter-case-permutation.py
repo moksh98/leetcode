17bf88a1245da99e060b5f77c676a9ba6d168c3a
Moksh Jain <mokshjain98@gmail.com>
2022-06-30 02:08:41 -0400

    Time: 99 ms (39.10%), Space: 15.4 MB (34.14%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.py
+++ 784-letter-case-permutation/784-letter-case-permutation.py
@@ -1,21 +1,37 @@
 class Solution:
     def letterCasePermutation(self, s: str) -> List[str]:
-        l = list(s)
         n = len(s)
         solution = []
-        def backtrack(start):
-            if start == n:
-                solution.append(self.joinList(l))
-            for i in range(start, n):
-                if l[i].isalpha():
-                    solution.append(self.joinList(l))
-                    l[i] = l[i].swapcase()
-                    backtrack(i+1)
-                    l[i] = l[i].swapcase()
-                else:
-                    solution.append(self.joinList(l))
-        backtrack(0)
-        return set(solution)
+        def backtrack(string, index):
+            if len(string) == n:
+                solution.append(string)
+                return
+            if s[index].isalpha():
+                backtrack(string+s[index], index+1)
+                backtrack(string+s[index].swapcase(), index+1)
+            else:
+                backtrack(string+s[index], index+1)
+        backtrack('', 0)
+        return solution
+                 
         
-    def joinList(self, l):
-        return ''.join(l)
+        
+#         l = list(s)
+#         n = len(s)
+#         solution = []
+#         def backtrack(start):
+#             if start == n:
+#                 solution.append(self.joinList(l))
+#             for i in range(start, n):
+#                 if l[i].isalpha():
+#                     solution.append(self.joinList(l))
+#                     l[i] = l[i].swapcase()
+#                     backtrack(i+1)
+#                     l[i] = l[i].swapcase()
+#                 else:
+#                     solution.append(self.joinList(l))
+#         backtrack(0)
+#         return set(solution)
+        
+#     def joinList(self, l):
+#         return ''.join(l)
